27dc23ffe08c2ad69e1b4d74bdf64389985f0540
Beatrice <beatrice.beacco@gmail.com>
2021-10-15 14:16:39 +0200

    fatto 18

--- part4/04bloglist_expansion/app.js
+++ part4/04bloglist_expansion/app.js
@@ -10,6 +10,7 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 //Connect to Mongoose
@@ -21,6 +22,7 @@ logger.info('connecting to', config.MONGODB_URI)
 //Use middleware
 app.use(cors())
 app.use(express.json())
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
