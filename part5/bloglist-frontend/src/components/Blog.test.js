23160515f7df55a3307720930944343300b557c3
Utente <utente@iMac-di-Michael.local>
2021-11-03 10:59:21 +0100

    fatto il 5-14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
  
 test('renders content', () => {
@@ -20,7 +21,7 @@ test('renders content', () => {
  
    expect(component.container).toHaveTextContent(
     'Testblog'
-   )
+  )
    expect(component.container).toHaveTextContent(
     'Tester'
   )
@@ -30,4 +31,36 @@ test('renders content', () => {
   expect(component.container).not.toHaveTextContent(
     'testblog.net'
   )
+})
+
+test('renders on click', () => {
+  const blog = {
+   title: 'Testblog',
+   author: 'Tester',
+   url: 'testblog.net',
+   likes: 1,
+   users: [{id: "61633ebc7eee58450b2983c6",
+   name: "Bea",
+   username: "Beuccia"}]
+  }
+
+  const username = {
+    name: 'Tester',
+    username: 'Tester'
+   }
+
+  const component = render(
+      <Blog blog={blog} user={username} />
+  )
+
+  const button = component.getByText('Show')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    'testblog.net'
+  )
+   expect(component.container).toHaveTextContent(
+    1
+  )
+  
 })
