4a1140a18e7b6577e1e1bdff3a4990e5446efb1a
(Bea)*2 <77938697+Beatrice-Beacco@users.noreply.github.com>
2021-12-31 12:49:32 +0100

    fatto fino a 8-9

---
+++ part8/02library-frontend/src/components/Authors.js
@@ -0,0 +1,55 @@
+  
+import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+		born
+    bookCount
+  }
+}
+`
+
+const Authors = (props) => {
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+    if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {result.data.allAuthors.map(author =>
+            <tr key={author.name}>
+              <td>{author.name}</td>
+              <td>{author.born}</td>
+              <td>{author.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
