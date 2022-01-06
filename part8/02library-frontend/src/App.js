9a719e59e1607e8bef9557782e902a641d056f3d
(Bea)*2 <77938697+Beatrice-Beacco@users.noreply.github.com>
2022-01-06 15:30:55 +0100

    fatto 8-24

--- part8/02library-frontend/src/App.js
+++ part8/02library-frontend/src/App.js
@@ -1,25 +1,34 @@
-
 import React, { useState, useEffect } from "react";
-import {useApolloClient } from "@apollo/client";
+import { useApolloClient, useSubscription } from "@apollo/client";
 
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
-import Recommended from './components/Recommended'
-import Login from './components/Login'
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import Recommended from "./components/Recommended";
+import Login from "./components/Login";
+
+import { BOOK_ADDED } from "./queries";
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState("authors");
   const [token, setToken] = useState(null);
 
   const client = useApolloClient();
 
+  useSubscription(BOOK_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      window.alert(
+        'Book "' + subscriptionData.data.bookAdded.title + '" was added.'
+      );
+    },
+  });
+
   useEffect(() => {
     const checkIfTokenExists = localStorage.getItem("phonenumbers-user-token");
     if (checkIfTokenExists) {
-      setToken(checkIfTokenExists)
+      setToken(checkIfTokenExists);
     }
-  },[])
+  }, []);
 
   const loginButton = () => (
     <>
@@ -28,51 +37,37 @@ const App = () => {
   );
   const loggedUserDisplayButtons = () => (
     <>
-    <button onClick={() => setPage("add")}>Add Book</button>
-    <button onClick={() => setPage("recoms")}>Reccommended</button>
-    <button onClick={() => logoutAction()}>Logout</button>
+      <button onClick={() => setPage("add")}>Add Book</button>
+      <button onClick={() => setPage("recoms")}>Reccommended</button>
+      <button onClick={() => logoutAction()}>Logout</button>
     </>
   );
 
   const logoutAction = () => {
-      setToken(null);
-      localStorage.clear();
-      client.resetStore();
-  }
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>Authors</button>
-        <button onClick={() => setPage('books')}>Books</button>
-        {token ? 
-        loggedUserDisplayButtons()
-        : loginButton()
-        }
+        <button onClick={() => setPage("authors")}>Authors</button>
+        <button onClick={() => setPage("books")}>Books</button>
+        {token ? loggedUserDisplayButtons() : loginButton()}
       </div>
 
-      <Authors
-        show={page === 'authors'}
-      />
+      <Authors show={page === "authors"} />
 
-      <Books
-        show={page === 'books'}
-      />
+      <Books show={page === "books"} />
 
-      <NewBook
-        show={page === 'add'}
-      /> 
+      <NewBook show={page === "add"} />
 
-      <Recommended 
-        show={page === 'recoms'}
-      />     
-      
-      <Login
-        show={page === 'login'}
-        tokenHandler= {setToken}
-      />
+      <Recommended show={page === "recoms"} />
+
+      <Login show={page === "login"} tokenHandler={setToken} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
