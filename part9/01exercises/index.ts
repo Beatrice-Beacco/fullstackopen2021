c52a7f106645a807c14049edab4c096ca6d5ba57
(Bea)*2 <77938697+Beatrice-Beacco@users.noreply.github.com>
2022-01-11 17:43:06 +0100

    finito 9-5

--- part9/01exercises/index.ts
+++ part9/01exercises/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
 const app = express();
 
+import calculateBmi from "./bmiCalculator";
+
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
 
 //endpoint bmi
 //input with query string parameters
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+
+  if (!height || !weight) res.status(400).json({ error: "Missing parameters" });
+  if (isNaN(Number(height)) || isNaN(Number(weight)))
+    res.status(400).json({ error: "Malformatted parameters" });
+
+  const bmi = calculateBmi(Number(height), Number(weight));
+
+  res.status(200).json({ height, weight, bmi });
+});
 
 const PORT = 3003;
 
